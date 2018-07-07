5b47c1d91ba7720f4463d2605e6436f34739bc7f
aviv.beeri <aviv.beeri@forgerock.com>
2018-07-07 18:24:11 +0100

    Moving STB files to /src/include and including in main

--- src/main.c
+++ src/main.c
@@ -13,7 +13,10 @@
 #define STBI_ONLY_JPEG
 #define STBI_ONLY_PNG
 #define STBI_ONLY_BMP
-#include "lib/stb_image.h"
+#include "include/stb_image.h"
+
+#define STB_TRUETYPE_IMPLEMENTATION
+#include "include/stb_truetype.h"
 
 #define internal static
 #define global_variable static
