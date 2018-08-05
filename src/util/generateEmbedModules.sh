e901ec2ec90b1f95020c211bc1f245c233e192c4
Aviv Beeri <avbeeri@gmail.com>
2018-08-05 16:57:16 +0100

    Added a script for embedding the internal engine modules inside the application

---
+++ src/util/generateEmbedModules.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+gcc embed.c -o embed -std=c99
+./embed ../engine/init.wren initModule ../engine/init.wren.inc
+./embed ../engine/input.wren inputModule ../engine/input.wren.inc
+./embed ../engine/graphics.wren graphicsModule ../engine/graphics.wren.inc
+./embed ../engine/io.wren ioModule ../engine/io.wren.inc
+./embed ../engine/audio.wren audioModule ../engine/audio.wren.inc
