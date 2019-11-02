65de3f1447a78e30702b1661ede82fcc587ab443
avivbeeri <avbeeri@gmail.com>
2019-11-02 08:06:16 +0000

    We round parameters rather than flooring.

--- src/modules/graphics.c
+++ src/modules/graphics.c
@@ -1,9 +1,9 @@
 internal void CANVAS_print(WrenVM* vm) {
   ENGINE* engine = (ENGINE*)wrenGetUserData(vm);
   char* text = (char*)wrenGetSlotString(vm, 1);
-  int16_t x = floor(wrenGetSlotDouble(vm, 2));
-  int16_t y = floor(wrenGetSlotDouble(vm, 3));
-  uint32_t c = floor(wrenGetSlotDouble(vm, 4));
+  int16_t x = round(wrenGetSlotDouble(vm, 2));
+  int16_t y = round(wrenGetSlotDouble(vm, 3));
+  uint32_t c = round(wrenGetSlotDouble(vm, 4));
 
   ENGINE_print(engine, text, x, y, c);
 }
@@ -11,81 +11,81 @@ internal void CANVAS_print(WrenVM* vm) {
 internal void CANVAS_pset(WrenVM* vm)
 {
   ENGINE* engine = (ENGINE*)wrenGetUserData(vm);
-  int16_t x = floor(wrenGetSlotDouble(vm, 1));
-  int16_t y = floor(wrenGetSlotDouble(vm, 2));
-  uint32_t c = floor(wrenGetSlotDouble(vm, 3));
+  int16_t x = round(wrenGetSlotDouble(vm, 1));
+  int16_t y = round(wrenGetSlotDouble(vm, 2));
+  uint32_t c = round(wrenGetSlotDouble(vm, 3));
   ENGINE_pset(engine, x,y,c);
 }
 
 internal void CANVAS_circle_filled(WrenVM* vm)
 {
   ENGINE* engine = (ENGINE*)wrenGetUserData(vm);
-  int16_t x = floor(wrenGetSlotDouble(vm, 1));
-  int16_t y = floor(wrenGetSlotDouble(vm, 2));
-  int16_t r = floor(wrenGetSlotDouble(vm, 3));
-  uint32_t c = floor(wrenGetSlotDouble(vm, 4));
+  int16_t x = round(wrenGetSlotDouble(vm, 1));
+  int16_t y = round(wrenGetSlotDouble(vm, 2));
+  int16_t r = round(wrenGetSlotDouble(vm, 3));
+  uint32_t c = round(wrenGetSlotDouble(vm, 4));
   ENGINE_circle_filled(engine, x, y, r, c);
 }
 
 internal void CANVAS_circle(WrenVM* vm)
 {
   ENGINE* engine = (ENGINE*)wrenGetUserData(vm);
-  int16_t x = floor(wrenGetSlotDouble(vm, 1));
-  int16_t y = floor(wrenGetSlotDouble(vm, 2));
-  int16_t r = floor(wrenGetSlotDouble(vm, 3));
-  uint32_t c = floor(wrenGetSlotDouble(vm, 4));
+  int16_t x = round(wrenGetSlotDouble(vm, 1));
+  int16_t y = round(wrenGetSlotDouble(vm, 2));
+  int16_t r = round(wrenGetSlotDouble(vm, 3));
+  uint32_t c = round(wrenGetSlotDouble(vm, 4));
   ENGINE_circle(engine, x, y, r, c);
 }
 internal void CANVAS_line(WrenVM* vm)
 {
   ENGINE* engine = (ENGINE*)wrenGetUserData(vm);
-  int16_t x1 = floor(wrenGetSlotDouble(vm, 1));
-  int16_t y1 = floor(wrenGetSlotDouble(vm, 2));
-  int16_t x2 = floor(wrenGetSlotDouble(vm, 3));
-  int16_t y2 = floor(wrenGetSlotDouble(vm, 4));
-  uint32_t c = floor(wrenGetSlotDouble(vm, 5));
+  int16_t x1 = round(wrenGetSlotDouble(vm, 1));
+  int16_t y1 = round(wrenGetSlotDouble(vm, 2));
+  int16_t x2 = round(wrenGetSlotDouble(vm, 3));
+  int16_t y2 = round(wrenGetSlotDouble(vm, 4));
+  uint32_t c = round(wrenGetSlotDouble(vm, 5));
   ENGINE_line(engine, x1, y1, x2, y2, c);
 }
 
 internal void CANVAS_ellipse(WrenVM* vm)
 {
   ENGINE* engine = (ENGINE*)wrenGetUserData(vm);
-  int16_t x1 = floor(wrenGetSlotDouble(vm, 1));
-  int16_t y1 = floor(wrenGetSlotDouble(vm, 2));
-  int16_t x2 = floor(wrenGetSlotDouble(vm, 3));
-  int16_t y2 = floor(wrenGetSlotDouble(vm, 4));
-  uint32_t c = floor(wrenGetSlotDouble(vm, 5));
+  int16_t x1 = round(wrenGetSlotDouble(vm, 1));
+  int16_t y1 = round(wrenGetSlotDouble(vm, 2));
+  int16_t x2 = round(wrenGetSlotDouble(vm, 3));
+  int16_t y2 = round(wrenGetSlotDouble(vm, 4));
+  uint32_t c = round(wrenGetSlotDouble(vm, 5));
   ENGINE_ellipse(engine, x1, y1, x2, y2, c);
 }
 
 internal void CANVAS_ellipsefill(WrenVM* vm)
 {
   ENGINE* engine = (ENGINE*)wrenGetUserData(vm);
-  int16_t x1 = floor(wrenGetSlotDouble(vm, 1));
-  int16_t y1 = floor(wrenGetSlotDouble(vm, 2));
-  int16_t x2 = floor(wrenGetSlotDouble(vm, 3));
-  int16_t y2 = floor(wrenGetSlotDouble(vm, 4));
-  uint32_t c = floor(wrenGetSlotDouble(vm, 5));
+  int16_t x1 = round(wrenGetSlotDouble(vm, 1));
+  int16_t y1 = round(wrenGetSlotDouble(vm, 2));
+  int16_t x2 = round(wrenGetSlotDouble(vm, 3));
+  int16_t y2 = round(wrenGetSlotDouble(vm, 4));
+  uint32_t c = round(wrenGetSlotDouble(vm, 5));
   ENGINE_ellipsefill(engine, x1, y1, x2, y2, c);
 }
 
 internal void CANVAS_rect(WrenVM* vm)
 {
   ENGINE* engine = (ENGINE*)wrenGetUserData(vm);
-  int16_t x = floor(wrenGetSlotDouble(vm, 1));
-  int16_t y = floor(wrenGetSlotDouble(vm, 2));
-  int16_t w = floor(wrenGetSlotDouble(vm, 3));
-  int16_t h = floor(wrenGetSlotDouble(vm, 4));
-  uint32_t c = floor(wrenGetSlotDouble(vm, 5));
+  int16_t x = round(wrenGetSlotDouble(vm, 1));
+  int16_t y = round(wrenGetSlotDouble(vm, 2));
+  int16_t w = round(wrenGetSlotDouble(vm, 3));
+  int16_t h = round(wrenGetSlotDouble(vm, 4));
+  uint32_t c = round(wrenGetSlotDouble(vm, 5));
   ENGINE_rect(engine, x, y, w, h, c);
 }
 internal void CANVAS_rectfill(WrenVM* vm)
 {
   ENGINE* engine = (ENGINE*)wrenGetUserData(vm);
-  int16_t x = floor(wrenGetSlotDouble(vm, 1));
-  int16_t y = floor(wrenGetSlotDouble(vm, 2));
-  int16_t w = floor(wrenGetSlotDouble(vm, 3));
-  int16_t h = floor(wrenGetSlotDouble(vm, 4));
-  uint32_t c = floor(wrenGetSlotDouble(vm, 5));
+  int16_t x = round(wrenGetSlotDouble(vm, 1));
+  int16_t y = round(wrenGetSlotDouble(vm, 2));
+  int16_t w = round(wrenGetSlotDouble(vm, 3));
+  int16_t h = round(wrenGetSlotDouble(vm, 4));
+  uint32_t c = round(wrenGetSlotDouble(vm, 5));
   ENGINE_rectfill(engine, x, y, w, h, c);
 }
