7ea8b83800a0d061444bd59470589d7cb760b0ca
avivbeeri <avbeeri@gmail.com>
2019-11-06 12:59:43 +0000

    Tweaking how Makefile and setup works to make switching between debug->release builds easier

--- setup.sh
+++ setup.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-MODE=${1:release}
-
 git submodule update
 cd src/lib/wren
-make WREN_OPT_RANDOM=1 MODE=$MODE vm
+make clean
+make WREN_OPT_RANDOM=1 MODE=debug vm && cp lib/libwrend.a ../libwrend.a
+make clean
+make WREN_OPT_RANDOM=1 MODE=release vm && cp lib/libwren.a ../libwren.a
 
-if [ $MODE == "debug" ]; then
-  cp lib/libwrend.a ../libwrend.a
-else
-  cp lib/libwren.a ../libwren.a
-fi
