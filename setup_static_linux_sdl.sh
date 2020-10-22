da6dfcc6cde5f6e715e6b57cca2fe32d14b10164
avivbeeri <avbeeri@gmail.com>
2020-10-22 10:38:20 +0100

    Experimental static build

--- setup_static_linux_sdl.sh
+++ setup_static_linux_sdl.sh
@@ -12,18 +12,19 @@ fi
 if ! [ -d "$DIRECTORY/$FOLDER" ]; then
   cd $DIRECTORY
   mkdir ${FOLDER} ; cd ${FOLDER}
-  cmake -DSDL_SHARED=OFF -DSDL_TEST=OFF -DSDL_STATIC=ON -DJACK_SHARED=OFF -DPULSEAUDIO_SHARED=OFF -DALSA_SHARED=OFF -DSNDIO=OFF ..
+  # cmake -DSDL_SHARED=OFF -DSDL_TEST=OFF -DSDL_STATIC=ON -DJACK_SHARED=OFF -DPULSEAUDIO_SHARED=OFF -DALSA_SHARED=OFF -DSNDIO=OFF ..
+  ../configure --disable-shared --enable-wayland-shared --enable-x11-shared
 else
   cd $DIRECTORY/${FOLDER}
 fi
 
 make
 
-if [ -f "$DIRECTORY/$FOLDER/libSDL2main.a" ]; then
-  cp $DIRECTORY/${FOLDER}/libSDL2main.a $DOME_DIR/lib
+if [ -f "$DIRECTORY/$FOLDER/build/.libs/libSDL2main.a" ]; then
+  cp $DIRECTORY/${FOLDER}/build/.libs/libSDL2main.a $DOME_DIR/lib
 fi
 
-cp $DIRECTORY/${FOLDER}/libSDL2.a $DOME_DIR/lib
+cp $DIRECTORY/${FOLDER}/build/.libs/libSDL2.a $DOME_DIR/lib
 cp $DIRECTORY/${FOLDER}/sdl2-config $DOME_DIR/lib/sdl2-config
 chmod +x $DOME_DIR/lib/sdl2-config
 
