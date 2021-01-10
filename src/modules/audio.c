015daa8c7a101379bcc071b755f402b0e8007c49
avivbeeri <avbeeri@gmail.com>
2021-01-10 17:26:39 +0000

    Created a special audio subfolder for compilation

--- src/modules/audio.c
+++ src/modules/audio.c
@@ -1,134 +1,11 @@
-internal void
-AUDIO_CHANNEL_finish(WrenVM* vm, CHANNEL* base) {
-  AUDIO_CHANNEL* channel = (AUDIO_CHANNEL*)CHANNEL_getData(base);
-  assert(channel != NULL);
-  if (channel->audioHandle != NULL) {
-    wrenReleaseHandle(vm, channel->audioHandle);
-    channel->audioHandle = NULL;
-  }
-  free(channel->soundId);
-  free(channel);
-}
-
-internal void
-AUDIO_CHANNEL_commit(AUDIO_CHANNEL* channel) {
-  size_t position = channel->current.position;
-  if (channel->new.resetPosition) {
-    position = channel->new.position;
-    channel->new.resetPosition = false;
-  }
-  channel->current = channel->new;
-  channel->current.position = position;
-  channel->new = channel->current;
-}
-
-internal void
-AUDIO_CHANNEL_update(WrenVM* vm, CHANNEL* base) {
-  AUDIO_CHANNEL* channel = (AUDIO_CHANNEL*)CHANNEL_getData(base);
-  switch (CHANNEL_getState(base)) {
-    case CHANNEL_INITIALIZE:
-      CHANNEL_setState(base, CHANNEL_TO_PLAY);
-      // Fallthrough
-    case CHANNEL_DEVIRTUALIZE:
-    case CHANNEL_TO_PLAY:
-      if (CHANNEL_getState(base) == CHANNEL_DEVIRTUALIZE) {
-        // We might do special things to de-virtualize a channel
-      }
-      if (channel->audio == NULL) {
-        CHANNEL_setState(base, CHANNEL_LOADING);
-        break;
-      }
-      // We assume data is loaded by now.
-      CHANNEL_setState(base, CHANNEL_PLAYING);
-      AUDIO_CHANNEL_commit(channel);
-      break;
-    case CHANNEL_LOADING:
-      if (channel->audio != NULL) {
-        CHANNEL_setState(base, CHANNEL_TO_PLAY);
-      }
-      break;
-    case CHANNEL_PLAYING:
-      AUDIO_CHANNEL_commit(channel);
-      if (CHANNEL_getEnabled(base) == false || CHANNEL_hasStopRequested(base)) {
-        CHANNEL_setState(base, CHANNEL_STOPPING);
-      }
-      break;
-    case CHANNEL_STOPPING:
-      if (channel->fade) {
-        channel->new.volume -= 0.1;
-      } else {
-        channel->new.volume = 0;
-      }
-      if (channel->new.volume <= 0) {
-        channel->new.volume = 0;
-        CHANNEL_setState(base, CHANNEL_STOPPED);
-      }
-      AUDIO_CHANNEL_commit(channel);
-      break;
-    case CHANNEL_STOPPED:
-      CHANNEL_setEnabled(base, false);
-      AUDIO_CHANNEL_commit(channel);
-      break;
-    default: break;
-  }
-}
-
-internal void
-AUDIO_CHANNEL_mix(CHANNEL* base, float* stream, size_t totalSamples) {
-  AUDIO_CHANNEL* channel = (AUDIO_CHANNEL*)CHANNEL_getData(base);
-  if (channel->audio == NULL) {
-    return;
-  }
-  AUDIO_DATA* audio = channel->audio;
-  float* startReadCursor = (float*)(audio->buffer);
-  float* readCursor = startReadCursor + channel->current.position * channels;
-  float* writeCursor = stream;
-  size_t length = audio->length;
-
-  size_t samplesToWrite = channel->current.loop ? totalSamples : min(totalSamples, length - channel->current.position);
-  float volume = channel->current.volume;
-  float targetPan = channel->current.pan;
-  float actualVolume = channel->actualVolume;
-  float actualPan = channel->actualPan;
-
-
-  for (size_t i = 0; i < samplesToWrite; i++) {
-    // We have to lerp the volume and pan change across the whole sample buffer
-    // or we get a clicking sound.
-    float f = i / (float)samplesToWrite;
-    float currentVolume = lerp(actualVolume, volume, f);
-    float currentPan = lerp(actualPan, targetPan, f);
-    float pan = (currentPan + 1.0f) * M_PI / 4.0f; // Channel pan is [-1,1] real pan needs to be [0,1]
-
-    // We have to advance the cursor after each read and write
-    // Read/Write left
-    *(writeCursor++) += *(readCursor++) * cos(pan) * currentVolume;
-    // Read/Write right
-    *(writeCursor++) += *(readCursor++) * sin(pan) * currentVolume;
-
-    channel->current.position++;
-    if (channel->current.position >= length) {
-      if (channel->current.loop) {
-        channel->current.position = 0;
-        readCursor = startReadCursor;
-      } else {
-        break;
-      }
-    }
-  }
-  channel->actualVolume = channel->current.volume;
-  channel->actualPan = channel->current.pan;
-  CHANNEL_setEnabled(base, channel->current.loop || channel->current.position < length);
-}
-
-
 internal float* resample(float* data, size_t srcLength, uint64_t srcFrequency, uint64_t targetFrequency, size_t* destLength);
+
 internal void
 AUDIO_allocate(WrenVM* vm) {
   wrenEnsureSlots(vm, 1);
   AUDIO_DATA* data = (AUDIO_DATA*)wrenSetSlotNewForeign(vm, 0, 0, sizeof(AUDIO_DATA));
   int length;
-  ASSERT_SLOT_TYPE(vm, 1, STRING, "buffer");
+  ASSERT_SLOT_TYPE(vm, 1, STRING, "fbuffer");
   const char* fileBuffer = wrenGetSlotBytes(vm, 1, &length);
 
   int16_t* tempBuffer;
@@ -199,51 +76,6 @@ AUDIO_allocate(WrenVM* vm) {
   }
 }
 
-internal float*
-resample(float* data, size_t srcLength, uint64_t srcFrequency, uint64_t targetFrequency, size_t* destLength) {
-  // Compute GCD of both frequencies
-  uint64_t divisor = gcd(srcFrequency, targetFrequency);
-
-  uint64_t L = targetFrequency / divisor;
-  uint64_t M = srcFrequency / divisor;
-
-  size_t sampleCount = srcLength;
-
-  size_t tempSampleCount = sampleCount * L;
-  float* tempData = calloc(tempSampleCount, bytesPerSample);
-  if (tempData == NULL) {
-    return NULL;
-  }
-
-  size_t destSampleCount = ceil(tempSampleCount / M) + 1;
-  *destLength = destSampleCount;
-  float* destData = malloc(destSampleCount * bytesPerSample);
-  if (destData == NULL) {
-    return NULL;
-  }
-  // Space out samples in temp data
-  float* sampleCursor = data;
-  float* writeCursor = tempData;
-  for (size_t i = 0; i < sampleCount * L; i++) {
-    size_t index = channels * (i / L);
-    *(writeCursor++) = sampleCursor[index];
-    *(writeCursor++) = sampleCursor[index + 1];
-  }
-
-  // TODO: Low-pass filter over the data (optional - but recommended)
-
-  // decimate by M
-  sampleCursor = tempData;
-  writeCursor = destData;
-
-  for(size_t i = 0; i < tempSampleCount; i += M) {
-    *(writeCursor++) = sampleCursor[i*2];
-    *(writeCursor++) = sampleCursor[i*2+1];
-  }
-
-  free(tempData);
-  return destData;
-}
 
 internal void
 AUDIO_finalize(void* data) {
@@ -427,14 +259,8 @@ AUDIO_CHANNEL_getPosition(WrenVM* vm) {
 
   ENGINE* engine = wrenGetUserData(vm);
   AUDIO_ENGINE* data = engine->audioEngine;
-
-  CHANNEL* base;
-  if (!AUDIO_ENGINE_get(data, ref, &base)) {
-    return;
-  }
-  AUDIO_CHANNEL* channel = CHANNEL_getData(base);
   wrenEnsureSlots(vm, 1);
-  wrenSetSlotDouble(vm, 0, channel->new.position);
+  wrenSetSlotDouble(vm, 0, AUDIO_ENGINE_getPosition(data, ref));
 }
 
 internal void
@@ -460,13 +286,8 @@ AUDIO_CHANNEL_setLoop(WrenVM* vm) {
   ENGINE* engine = wrenGetUserData(vm);
   AUDIO_ENGINE* data = engine->audioEngine;
 
-  CHANNEL* base;
-  if (!AUDIO_ENGINE_get(data, ref, &base)) {
-    return;
-  }
-  AUDIO_CHANNEL* channel = (AUDIO_CHANNEL*)CHANNEL_getData(base);
   ASSERT_SLOT_TYPE(vm, 1, BOOL, "loop");
-  channel->new.loop = wrenGetSlotBool(vm, 1);
+  AUDIO_ENGINE_setLoop(data, ref, wrenGetSlotBool(vm, 1));
 }
 
 internal void
@@ -475,15 +296,8 @@ AUDIO_CHANNEL_getLoop(WrenVM* vm) {
 
   ENGINE* engine = wrenGetUserData(vm);
   AUDIO_ENGINE* data = engine->audioEngine;
-
-  CHANNEL* base;
-  if (!AUDIO_ENGINE_get(data, ref, &base)) {
-    wrenSetSlotBool(vm, 0, false);
-    return;
-  }
-  AUDIO_CHANNEL* channel = (AUDIO_CHANNEL*)CHANNEL_getData(base);
   wrenEnsureSlots(vm, 1);
-  wrenSetSlotBool(vm, 0, channel->new.loop);
+  wrenSetSlotBool(vm, 0, AUDIO_ENGINE_getLoop(data, ref));
 }
 
 internal void
@@ -492,16 +306,8 @@ AUDIO_CHANNEL_setPosition(WrenVM* vm) {
 
   ENGINE* engine = wrenGetUserData(vm);
   AUDIO_ENGINE* data = engine->audioEngine;
-
-  CHANNEL* base;
-  if (!AUDIO_ENGINE_get(data, ref, &base)) {
-    return;
-  }
-  AUDIO_CHANNEL* channel = (AUDIO_CHANNEL*)CHANNEL_getData(base);
   ASSERT_SLOT_TYPE(vm, 1, NUM, "position");
-  size_t newPosition = round(wrenGetSlotDouble(vm, 1));
-  channel->new.position = mid(0, newPosition, channel->audio->length);
-  channel->new.resetPosition = true;
+  AUDIO_ENGINE_setPosition(data, ref, round(wrenGetSlotDouble(vm, 1)));
 }
 
 internal void
@@ -510,14 +316,8 @@ AUDIO_CHANNEL_setVolume(WrenVM* vm) {
 
   ENGINE* engine = wrenGetUserData(vm);
   AUDIO_ENGINE* data = engine->audioEngine;
-
-  CHANNEL* base;
-  if (!AUDIO_ENGINE_get(data, ref, &base)) {
-    return;
-  }
-  AUDIO_CHANNEL* channel = (AUDIO_CHANNEL*)CHANNEL_getData(base);
   ASSERT_SLOT_TYPE(vm, 1, NUM, "volume");
-  channel->new.volume = fmax(0, wrenGetSlotDouble(vm, 1));
+  AUDIO_ENGINE_setVolume(data, ref, wrenGetSlotDouble(vm, 1));
 }
 
 internal void
@@ -526,16 +326,9 @@ AUDIO_CHANNEL_getVolume(WrenVM* vm) {
 
   ENGINE* engine = wrenGetUserData(vm);
   AUDIO_ENGINE* data = engine->audioEngine;
-
-  CHANNEL* base;
-  if (!AUDIO_ENGINE_get(data, ref, &base)) {
-    wrenSetSlotDouble(vm, 0, 0);
-    return;
-  }
-  AUDIO_CHANNEL* channel = (AUDIO_CHANNEL*)CHANNEL_getData(base);
-  wrenEnsureSlots(vm, 1);
-  wrenSetSlotDouble(vm, 0, channel->new.volume);
+  wrenSetSlotDouble(vm, 0, AUDIO_ENGINE_getVolume(data, ref));
 }
+
 
 internal void
 AUDIO_CHANNEL_setPan(WrenVM* vm) {
@@ -544,14 +337,8 @@ AUDIO_CHANNEL_setPan(WrenVM* vm) {
   ENGINE* engine = wrenGetUserData(vm);
   AUDIO_ENGINE* data = engine->audioEngine;
 
-  CHANNEL* base;
-  if (!AUDIO_ENGINE_get(data, ref, &base)) {
-    return;
-  }
-  AUDIO_CHANNEL* channel = (AUDIO_CHANNEL*)CHANNEL_getData(base);
-
   ASSERT_SLOT_TYPE(vm, 1, NUM, "pan");
-  channel->new.pan = fmid(-1.0, wrenGetSlotDouble(vm, 1), 1.0f);
+  AUDIO_ENGINE_setPan(data, ref, wrenGetSlotDouble(vm, 1));
 }
 
 internal void
@@ -560,15 +347,8 @@ AUDIO_CHANNEL_getPan(WrenVM* vm) {
 
   ENGINE* engine = wrenGetUserData(vm);
   AUDIO_ENGINE* data = engine->audioEngine;
-
-  CHANNEL* base;
-  if (!AUDIO_ENGINE_get(data, ref, &base)) {
-    wrenSetSlotDouble(vm, 0, 0);
-    return;
-  }
-  AUDIO_CHANNEL* channel = (AUDIO_CHANNEL*)CHANNEL_getData(base);
   wrenEnsureSlots(vm, 1);
-  wrenSetSlotDouble(vm, 0, channel->new.pan);
+  wrenSetSlotDouble(vm, 0, AUDIO_ENGINE_getPan(data, ref));
 }
 
 internal void
@@ -583,3 +363,4 @@ internal double
 volumeToDb(double volume) {
   return 20.0 * log10(volume);
 }
+
