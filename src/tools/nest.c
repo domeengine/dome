5bf1f6b5926851fb67516e32c1f5d79c360b355d
avivbeeri <avbeeri@gmail.com>
2021-11-01 23:39:55 +0000

    Fiddling with various restrictions to NEST

--- src/tools/nest.c
+++ src/tools/nest.c
@@ -1,24 +1,35 @@
-typedef enum { MODE_NONE, MODE_ZIP, MODE_EXTRACT } MODE;
+typedef enum { MODE_NONE, MODE_CREATE, MODE_EXTRACT } MODE;
+bool DOT_FILES = false;
 
 void NEST_usage(ENGINE* engine) {
-  // TODO write usage
+  ENGINE_printLog(engine, "\nUsage: \n");
+  ENGINE_printLog(engine, "  dome nest [options] [--] (<file> | <directory>) ... \n");
+  ENGINE_printLog(engine, "\nOptions: \n");
+  ENGINE_printLog(engine, "  -c --create               Create a bundle from files.\n");
+  ENGINE_printLog(engine, "  -h --help                 Show this help message.\n");
+  ENGINE_printLog(engine, "  -o FILE, --output=FILE    The file to bundle into.\n");
+  ENGINE_printLog(engine, "  --include-dot-files       Include dot-prefixed files in bundle.\n");
+  ENGINE_printLog(engine, "\n");
 }
 
-int NEST_writeFile(ENGINE* engine, mtar_t* tar, char* filePath, size_t offset) {
+int NEST_writeFile(ENGINE* engine, mtar_t* tar, char* filePath, char* tarPath) {
   size_t length;
   char* inputFile = readEntireFile(filePath, &length);
   if (inputFile == NULL) {
     return EXIT_FAILURE;
   }
+  if (tarPath == NULL) {
+     tarPath = filePath;
+  }
   ENGINE_printLog(engine, "Bundling: %s\n", filePath);
-  mtar_write_file_header(tar, filePath + offset, length);
+  mtar_write_file_header(tar, tarPath, length);
   mtar_write_data(tar, inputFile, length);
   free(inputFile);
   return EXIT_SUCCESS;
 }
 
 int NEST_packDirectory(ENGINE* engine, mtar_t* tar, char* directory, size_t start) {
-  struct dirent* de;
+  struct dirent* de = NULL;
   DIR* dr = opendir(directory);
   if (dr == NULL) {
     ENGINE_printLog(engine, "dome: Could not open directory %s\n", directory);
@@ -26,6 +37,7 @@ int NEST_packDirectory(ENGINE* engine, mtar_t* tar, char* directory, size_t star
   }
 
   int result;
+
   while ((de = readdir(dr)) != NULL) {
     // Ignore the current and parent
     result = EXIT_SUCCESS;
@@ -36,15 +48,17 @@ int NEST_packDirectory(ENGINE* engine, mtar_t* tar, char* directory, size_t star
       strcat(path, "/");
       strcat(path, de->d_name);
 
-      if (strncmp(de->d_name, ".", 1) == 0) {
+      if (!DOT_FILES && strncmp(de->d_name, ".", 1) == 0) {
         continue;
       }
 
-      if (((de->d_type & DT_DIR) != 0)) {
+      if (de->d_type == DT_LNK) {
+        ENGINE_printLog(engine, "Ignoring symlink: %s\n", path);
+      } else if (((de->d_type & DT_DIR) != 0)) {
         // If this is a directory, but not a hidden one
         result = NEST_packDirectory(engine, tar, path, start);
       } else {
-        result = NEST_writeFile(engine, tar, path, start);
+        result = NEST_writeFile(engine, tar, path, path + start);
       }
       if (result == EXIT_FAILURE) {
         return EXIT_FAILURE;
@@ -61,10 +75,11 @@ int NEST_perform(ENGINE* engine, char **argv) {
   optparse_init(&options, argv);
   options.permute = 0;
   struct optparse_long longopts[] = {
-    {"help", 'h', OPTPARSE_NONE},
-    {"extract", 'x', OPTPARSE_NONE},
-    {"bundle", 'z', OPTPARSE_NONE},
+    {"create", 'c', OPTPARSE_NONE},
     {"output", 'o', OPTPARSE_REQUIRED},
+    {"extract", 'x', OPTPARSE_NONE},
+    {"help", 'h', OPTPARSE_NONE},
+    {"include-dot-files", 128, OPTPARSE_NONE},
     {0}
   };
   int option;
@@ -72,6 +87,7 @@ int NEST_perform(ENGINE* engine, char **argv) {
   char* outputFileName = "game.egg";
   while ((option = optparse_long(&options, longopts, NULL)) != -1) {
     switch (option) {
+      case 128: DOT_FILES = true; break;
       case 'h':
         NEST_usage(engine);
         return EXIT_SUCCESS;
@@ -81,11 +97,11 @@ int NEST_perform(ENGINE* engine, char **argv) {
         }
         mode = MODE_EXTRACT;
         break;
-      case 'z':
+      case 'c':
         if (mode != MODE_NONE) {
           return EXIT_FAILURE;
         }
-        mode = MODE_ZIP;
+        mode = MODE_CREATE;
         break;
       case 'o':
         outputFileName = strdup(options.optarg);
@@ -110,22 +126,29 @@ int NEST_perform(ENGINE* engine, char **argv) {
 
   // For now, we only support ZIP mode so we default to it.
   if (mode == MODE_NONE) {
-    mode = MODE_ZIP;
+    mode = MODE_CREATE;
   }
 
   bool singleFile = (argc == 1);
-  if (mode == MODE_ZIP) {
+  if (mode == MODE_CREATE) {
     mtar_t tar;
     mtar_open(&tar, outputFileName, "w");
     char* path = NULL;
     while ((path = optparse_arg(&options))) {
       /* Is this a file or directory? */
-      struct stat path_stat;
-      stat(path, &path_stat);
-      bool isFile = S_ISREG(path_stat.st_mode);
-      if (isFile) {
+      struct stat info;
+      stat(path, &info);
+      bool isLink = S_ISLNK(info.st_mode);
+      bool isFile = S_ISREG(info.st_mode);
+      if (isLink) {
+        ENGINE_printLog(engine, "Ignoring symlink: %s\n", path);
+        continue;
+      } else if (isFile) {
+        if (!DOT_FILES && path[0] == '.' && path[1] != '.' ) {
+          continue;
+        }
         /* File's get added */
-        NEST_writeFile(engine, &tar, path, 0);
+        NEST_writeFile(engine, &tar, path, NULL);
       } else {
         /* recurse into directories */
         int last = strlen(path) - 1;
@@ -142,7 +165,9 @@ int NEST_perform(ENGINE* engine, char **argv) {
     mtar_finalize(&tar);
     /* Close archive */
     mtar_close(&tar);
+    ENGINE_printLog(engine, "Created bundle %s.\n", outputFileName);
   }
+
 
   return EXIT_SUCCESS;
 }
