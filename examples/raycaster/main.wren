221216f21b43119c8177053a9fa54e4fa8d8a11b
avivbeeri <avbeeri@gmail.com>
2021-11-08 11:26:37 +0000

    Basic raycaster code

--- examples/raycaster/main.wren
+++ examples/raycaster/main.wren
@@ -1,4 +1,8 @@
+import "math" for Vec
 import "plugin" for Plugin
+import "input" for Keyboard
+import "graphics" for Canvas
+import "dome" for Process, Window
 
 Plugin.load("raycaster")
 // The plugin will be initialised now
@@ -7,16 +11,50 @@ Plugin.load("raycaster")
 import "raycaster" for Raycaster
 
 
+var SPEED = 0.1
+
 class Main {
   construct new() {}
 
-  init() {
-    // and allocators for foreign classes
-    _raycaster = Raycaster.init()
+  compute() {
+    var rads = _angle * Num.pi / 180
+    _dir = Vec.new(rads.cos, rads.sin)
   }
 
-  update() {}
+  init() {
+    Window.lockstep = true
+    _pos = Vec.new(22, 12)
+    _angle = 180
+    compute()
+
+    // and allocators for foreign classes
+    _raycaster = Raycaster.init()
+    _raycaster.setPosition(_pos.x, _pos.y)
+    _raycaster.setAngle(_angle)
+  }
+
+  update() {
+    if (Keyboard["up"].down) {
+      _pos = _pos + _dir*SPEED
+      _raycaster.setPosition(_pos.x, _pos.y)
+    }
+    if (Keyboard["down"].down) {
+      _pos = _pos - _dir*SPEED
+      _raycaster.setPosition(_pos.x, _pos.y)
+    }
+    if (Keyboard["left"].down) {
+      _angle = _angle - 0.5
+      compute()
+      _raycaster.setAngle(_angle)
+    }
+    if (Keyboard["right"].down) {
+      _angle = _angle + 0.5
+      compute()
+      _raycaster.setAngle(_angle)
+    }
+  }
   draw(alpha) {
+    Canvas.cls()
     _raycaster.draw(alpha)
   }
 }
--- examples/raycaster/raycaster.c
+++ examples/raycaster/raycaster.c
@@ -6,6 +6,7 @@
 static GRAPHICS_API_v0* graphics;
 static DOME_API_v0* core;
 static WREN_API_v0* wren;
+static void (*unsafePset)(DOME_Context, int32_t, int32_t, DOME_Color) = NULL;
 
 static uint32_t WIDTH = 0;
 static uint32_t HEIGHT = 0;
@@ -21,15 +22,79 @@ typedef struct {
 
 } RENDERER;
 
+#define mapWidth 24
+#define mapHeight 24
+#define screenWidth 640
+#define screenHeight 480
+
+int worldMap[mapWidth][mapHeight]=
+{
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+
+double posX = 22, posY = 12;  //x and y start position
+double dirX = -1, dirY = 0; //initial direction vector
+double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
+
 static const char* source =  ""
 "foreign class Raycaster {\n" // Source file for an external module
   "construct init() {} \n"
+  "foreign setPosition(x, y) \n"
+  "foreign setAngle(angle) \n"
   "foreign draw(alpha) \n"
 "} \n";
 
 void allocate(WrenVM* vm) {
   size_t CLASS_SIZE = sizeof(RENDERER); // This should be the size of your object's data
   void* obj = wren->setSlotNewForeign(vm, 0, 0, CLASS_SIZE);
+}
+
+void setPosition(WrenVM* vm) {
+  double x = wren->getSlotDouble(vm, 1);
+  double y = wren->getSlotDouble(vm, 2);
+  posX = x;
+  posY = y;
+}
+void setAngle(WrenVM* vm) {
+  double angle = wren->getSlotDouble(vm, 1);
+  double rads = angle * M_PI / 180.0;
+  dirX = cos(rads);
+  dirY = sin(rads);
+  planeX = -dirY;
+  planeY = dirX;
+}
+
+void vLine(DOME_Context ctx, int32_t x, int32_t y0, uint32_t y1, uint32_t c) {
+  DOME_Color color;
+  color.value = c;
+  y0 = fmax(0, y0);
+  y1 = fmin(y1, HEIGHT);
+  for (int y = y0; y <= y1; y++) {
+    unsafePset(ctx, x, y, color);
+  }
 }
 
 void draw(WrenVM* vm) {
@@ -39,12 +104,115 @@ void draw(WrenVM* vm) {
   // Retrieve the DOME Context from the VM. This is needed for many things.
   DOME_Context ctx = core->getContext(vm);
   DOME_Color color;
-  color.value = 0xFFBBCCDD;
+  /*
+  color.value = 0xFF000000;
 
   for (int y = 0; y < HEIGHT; y++) {
     for (int x = 0; x < WIDTH; x++) {
-      graphics->unsafePset(ctx, x, y, color);
+      unsafePset(ctx, x, y, color);
     }
+  }
+  */
+
+  int w = WIDTH;
+  int h = HEIGHT;
+
+  for(int x = 0; x < w; x++) {
+    //calculate ray position and direction
+    double cameraX = 2 * x / ((double)w) - 1; //x-coordinate in camera space
+    double rayDirX = dirX + planeX * cameraX;
+    double rayDirY = dirY + planeY * cameraX;
+    //which box of the map we're in
+
+    int mapX = posX;
+    int mapY = posY;
+
+    //length of ray from current position to next x or y-side
+    double sideDistX;
+    double sideDistY;
+
+    //length of ray from one x or y-side to next x or y-side
+    double deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1 / rayDirX);
+    double deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1 / rayDirY);
+    double perpWallDist;
+
+    //what direction to step in x or y-direction (either +1 or -1)
+    int stepX;
+    int stepY;
+
+    int hit = 0; //was there a wall hit?
+    int side; //was a NS or a EW wall hit?
+
+
+    //calculate step and initial sideDist
+      if (rayDirX < 0)
+      {
+        stepX = -1;
+        sideDistX = (posX - mapX) * deltaDistX;
+      }
+      else
+      {
+        stepX = 1;
+        sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+      }
+      if (rayDirY < 0)
+      {
+        stepY = -1;
+        sideDistY = (posY - mapY) * deltaDistY;
+      }
+      else
+      {
+        stepY = 1;
+        sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+      }
+
+      //perform DDA
+      while (hit == 0)
+      {
+        //jump to next map square, either in x-direction, or in y-direction
+        if (sideDistX < sideDistY)
+        {
+          sideDistX += deltaDistX;
+          mapX += stepX;
+          side = 0;
+        }
+        else
+        {
+          sideDistY += deltaDistY;
+          mapY += stepY;
+          side = 1;
+        }
+        //Check if ray has hit a wall
+        if (worldMap[mapX][mapY] > 0) hit = 1;
+      }
+
+      //Calculate distance projected on camera direction (Euclidean distance would give fisheye effect!)
+      if(side == 0) perpWallDist = (sideDistX - deltaDistX);
+      else          perpWallDist = (sideDistY - deltaDistY);
+
+      //Calculate height of line to draw on screen
+      int lineHeight = (int)(h / perpWallDist);
+
+      //calculate lowest and highest pixel to fill in current stripe
+      int drawStart = -lineHeight / 2 + h / 2;
+      if(drawStart < 0)drawStart = 0;
+      int drawEnd = lineHeight / 2 + h / 2;
+      if(drawEnd >= h)drawEnd = h - 1;
+
+      uint32_t color;
+      //choose wall color
+      switch(worldMap[mapX][mapY])
+      {
+        case 1:  color = 0xFFFF0000;  break; //red
+        case 2:  color = 0xFF00FF00;  break; //green
+        case 3:  color = 0xFF0000FF;   break; //blue
+        case 4:  color = 0xFFFFFFFF;  break; //white
+        default: color = 0xFF00FFFF; break; //yellow
+      }
+
+      //give x and y sides different brightness
+      if (side == 1) {color = color / 2;}
+      vLine(ctx, x, drawStart, drawEnd, color);
   }
 }
 
@@ -54,6 +222,7 @@ DOME_EXPORT DOME_Result PLUGIN_onInit(DOME_getAPIFunction DOME_getAPI,
   // Fetch the latest Core API and save it for later use.
   core = DOME_getAPI(API_DOME, DOME_API_VERSION);
   graphics = DOME_getAPI(API_GRAPHICS, GRAPHICS_API_VERSION);
+  unsafePset = graphics->unsafePset;
 
   // DOME also provides a subset of the Wren API for accessing slots
   // in foreign methods.
@@ -67,9 +236,12 @@ DOME_EXPORT DOME_Result PLUGIN_onInit(DOME_getAPIFunction DOME_getAPI,
 
   core->registerClass(ctx, "raycaster", "Raycaster", allocate, NULL);
   core->registerFn(ctx, "raycaster", "Raycaster.draw(_)", draw);
+  core->registerFn(ctx, "raycaster", "Raycaster.setAngle(_)", setAngle);
+  core->registerFn(ctx, "raycaster", "Raycaster.setPosition(_,_)", setPosition);
 
   WIDTH = graphics->getWidth(ctx);
   HEIGHT = graphics->getHeight(ctx);
+
 
   // Returning anything other than SUCCESS here will result in the current fiber
   // aborting. Use this to indicate if your plugin initialised successfully.
