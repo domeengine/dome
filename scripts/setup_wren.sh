4e8b3d04b3c59531ada59a23c9e8348e3d9b7269
avivbeeri <avbeeri@gmail.com>
2021-12-06 22:25:10 +0000

    Revert "Removing redundant script now that we use the amalgamated build"
    This reverts commit a492bd16292a9e9c1ad553d7a8f75c966b46886f.

---
+++ scripts/setup_wren.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+DOME_DIR=$PWD
+INCLUDE_DIR=$DOME_DIR/include
+LIB_DIR=$DOME_DIR/lib
+WREN_DIR=$LIB_DIR/wren
+
+cd "$WREN_DIR/projects"
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  # ...
+  cd make
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  cd make.mac
+  # Mac OSX
+elif [[ "$OSTYPE" == "msys" ]]; then
+  cd make
+  # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+elif [[ "$OSTYPE" == "win32" ]]; then
+  exit 1
+else
+  exit 1
+fi
+
+# Undo external makefile flags just in case
+unset config
+MAKEFLAGS="--no-print-directory"
+# build the debug version of wren
+make clean
+CFLAGS=-fvisibility=hidden
+make ${@:2} CFLAGS=${CFLAGS} verbose=1 config=debug_$1 wren && cp "$WREN_DIR/lib/libwren_d.a" "$LIB_DIR/libwrend.a"
+# build the release version of wren
+make clean
+make ${@:2} CFLAGS=${CFLAGS} verbose=1 config=release_$1 wren && cp "$WREN_DIR/lib/libwren.a" "$LIB_DIR/libwren.a"
+# Copy the wren.h to our includes
+cp "$WREN_DIR/src/include/wren.h" "$INCLUDE_DIR/wren.h"
