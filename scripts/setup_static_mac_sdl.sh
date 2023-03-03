f5a03d710916ca3a210fbefcbf70b559c95a114e
Aviv Beeri <avbeeri@gmail.com>
2023-03-03 09:31:24 +0000

    fixing  sdlconfig path

--- scripts/setup_static_mac_sdl.sh
+++ scripts/setup_static_mac_sdl.sh
@@ -20,7 +20,7 @@ fi
 cp $DIRECTORY/${FOLDER}/build/.libs/libSDL2.a $LIB_DIR
 cp $DIRECTORY/${FOLDER}/build/.libs/libSDL2-2.0.0.dylib $LIB_DIR
 cp $DIRECTORY/${FOLDER}/build/.libs/libSDL2.dylib $LIB_DIR
-cp $DIRECTORY/sdl2-config $LIB_DIR/sdl2-config
+cp $DIRECTORY/${FOLDER}/sdl2-config $LIB_DIR/sdl2-config
 
 cp -r $DIRECTORY/include $INCLUDE_DIR/SDL2
 cp -r $DIRECTORY/${FOLDER}/include/SDL_config.h  $INCLUDE_DIR/SDL2/SDL_config.h
