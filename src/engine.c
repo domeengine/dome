445389f9f438d6d58e7a3eb7bb70bf9bb592420a
Aviv Beeri <avbeeri@gmail.com>
2023-03-12 07:35:11 +0000

    applying nodpi hint

--- src/engine.c
+++ src/engine.c
@@ -237,8 +237,10 @@ ENGINE_start(ENGINE* engine) {
   int result = EXIT_SUCCESS;
 
   uint32_t windowFlags = SDL_WINDOW_HIDDEN | SDL_WINDOW_RESIZABLE;
-#if defined _WIN32 || defined __MINGW32__
+#if defined (_WIN32) || defined (__MINGW32__)
   SDL_setenv("SDL_AUDIODRIVER", "directsound", true);
+  SDL_SetHint(SDL_HINT_MOUSE_RELATIVE_MODE_WARP, "1");
+  SDL_SetHint(SDL_HINT_VIDEO_HIGHDPI_DISABLED, "1");
 #else
   windowFlags |= SDL_WINDOW_ALLOW_HIGHDPI;
 #endif
